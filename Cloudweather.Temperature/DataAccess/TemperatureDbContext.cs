a25a58ab50709713306fbf0cea429c5e3bbfb818
Fabricio Silva <fabriciosilvalp@outlook.com>
2023-01-26 17:32:41 -0300

    Add data access to remaining microservices

---
+++ Cloudweather.Temperature/DataAccess/TemperatureDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Cloudweather.Temperature.DataAccess;
+
+public class TemperatureDbContext : DbContext
+{
+    public TemperatureDbContext()
+    {
+        
+    }
+
+    public TemperatureDbContext(DbContextOptions opts) : base(opts)
+    {
+        
+    }
+    
+    public DbSet<Temperature> Temperature { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+    
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Temperature>(b => b.ToTable("temperature"));
+    }
+    
+    
+}
