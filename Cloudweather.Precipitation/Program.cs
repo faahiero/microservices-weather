8cd1443eb377bc84130288f3067b8b380c7b7fa9
Fabricio Silva <fabriciosilvalp@outlook.com>
2023-01-26 17:05:46 -0300

    Add data access to preciptation service

--- Cloudweather.Precipitation/Program.cs
+++ Cloudweather.Precipitation/Program.cs
@@ -1,12 +1,34 @@
+using Cloudweather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+builder.Services.AddDbContext<PrecipDbContext>(opts =>
+{
+    opts.EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+        .UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+}, ServiceLifetime.Transient);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async(string zip, [FromQuery] int? days, PrecipDbContext dbContext) =>
 {
-    return Results.Ok(zip);
+    if (days is null or < 1 or > 30)
+    {
+        return Results.BadRequest("Please provide a valid number of days between 1 and 30");
+    }
+    
+    var startData = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await dbContext.Precipitation
+        .Where(p => p.ZipCode == zip && p.CreatedOn > startData)
+        .ToListAsync();
+    
+    return Results.Ok(results);
+
 });
 
 app.Run();
